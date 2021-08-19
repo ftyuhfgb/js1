fc74beeabaf3418a3dce44cbf4aa1a976b0e8fba
muxammadqodir <qosimovmuxammadqodir215@gmail.com>
2021-08-19 11:01:50 +0500

    js1

---
+++ script.js
@@ -0,0 +1,22 @@
+var str = prompt("Введите своё имя");
+console.log(str);
+var a = prompt('Введите фамилию')
+console.log(a);
+var age = prompt('Введите год рождения')
+console.log(age);
+var str= +prompt('Решите сложения 345+456=')
+console.log('Пример №1 345+456=801 Вaш ответ:');
+console.log(str);
+var str= +prompt('Решите вычитания 23-13=')
+console.log('Пример №2 23-13=10 Ваш ответ:');
+console.log(str);
+var str= +prompt('Решить умножения 34*2=')
+console.log('Пример №3 34*2=68 Ваш ответ:');
+console.log(str);
+var str= +prompt('Решить деление 890/5=')
+console.log('Пример №4 890/5=178 Ваш ответ:');
+console.log(str);
+var str= +prompt('Решить деление с остатком 555%6=')
+console.log('Пример №5 555%6=92,5 Ваш ответ:');
+console.log(str);
+
